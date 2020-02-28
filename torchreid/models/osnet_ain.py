c8ecda3d99f58302e7cf9b660564533d7d70f71d
Evgeny Izutov <evgeny.izutov@intel.com>
2020-02-28 12:25:57 +0300

    Set to default

--- torchreid/models/osnet_ain.py
+++ torchreid/models/osnet_ain.py
@@ -448,7 +448,7 @@ class OSNet(nn.Module):
             channels[3], self.use_attentions[2]
         )
         self.conv5 = Conv1x1(channels[3], channels[3])
-        self.global_avgpool = nn.AdaptiveMaxPool2d(1)
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
 
         self.fc = self._construct_fc_layer(self.feature_dim, channels[3], dropout_p=None)
 
