e3b66fe44d2de9cbcbda2f44b2b2060fdb124448
Evgeny Izutov <evgeny.izutov@intel.com>
2020-03-30 13:00:30 +0300

    Minor

--- torchreid/models/osnet.py
+++ torchreid/models/osnet.py
@@ -1,8 +1,15 @@
 from __future__ import division, absolute_import
+
 import warnings
+from collections import OrderedDict
+
 import torch
-from torch import nn
-from torch.nn import functional as F
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchreid.losses import AngleSimpleLinear
+from torchreid.ops import Dropout
+
 
 __all__ = [
     'osnet_x1_0', 'osnet_x0_75', 'osnet_x0_5', 'osnet_x0_25', 'osnet_ibn_x1_0'
@@ -25,6 +32,7 @@ pretrained_urls = {
 ##########
 # Basic layers
 ##########
+
 class ConvLayer(nn.Module):
     """Convolution layer (conv + bn + relu)."""
 
@@ -159,6 +167,7 @@ class LightConv3x3(nn.Module):
 ##########
 # Building blocks for omni-scale feature learning
 ##########
+
 class ChannelGate(nn.Module):
     """A mini-network that generates channel-wise gates conditioned on input tensor."""
 
@@ -279,6 +288,7 @@ class OSBlock(nn.Module):
 ##########
 # Network architecture
 ##########
+
 class OSNet(nn.Module):
     """Omni-Scale Network.
     
@@ -294,16 +304,23 @@ class OSNet(nn.Module):
         blocks,
         layers,
         channels,
+        IN=False,
         feature_dim=512,
         loss='softmax',
-        IN=False,
+        attr_tasks=None,
+        enable_attr_tasks=False,
+        num_parts=None,
         **kwargs
     ):
         super(OSNet, self).__init__()
+
         num_blocks = len(blocks)
         assert num_blocks == len(layers)
         assert num_blocks == len(channels) - 1
+
         self.loss = loss
+        self.feature_dim = feature_dim
+        assert self.feature_dim is not None and self.feature_dim > 0
 
         # convolutional backbone
         self.conv1 = ConvLayer(3, channels[0], 7, stride=2, padding=3, IN=IN)
@@ -330,29 +347,62 @@ class OSNet(nn.Module):
             channels[3],
             reduce_spatial_size=False
         )
-        self.conv5 = Conv1x1(channels[3], channels[3])
-        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
-        # fully connected layer
-        self.fc = self._construct_fc_layer(
-            feature_dim, channels[3], dropout_p=None
-        )
-        # identity classification layer
-        self.classifier = nn.Linear(self.feature_dim, num_classes)
+
+        out_num_channels = channels[3]
+        self.conv5 = Conv1x1(channels[3], out_num_channels)
+
+        if isinstance(num_classes, (list, tuple)):
+            assert len(num_classes) == 2
+            real_data_num_classes, synthetic_data_num_classes = num_classes
+        else:
+            real_data_num_classes, synthetic_data_num_classes = num_classes, None
+
+        classifier_block = nn.Linear if self.loss not in ['am_softmax'] else AngleSimpleLinear
+        self.num_parts = num_parts if num_parts is not None and num_parts > 1 else 0
+
+        if self.num_parts > 1:
+            self.part_self_fc = nn.ModuleList()
+            self.part_rest_fc = nn.ModuleList()
+            self.part_cat_fc = nn.ModuleList()
+            for _ in range(self.num_parts):
+                self.part_self_fc.append(self._construct_fc_layer(out_num_channels, out_num_channels))
+                self.part_rest_fc.append(self._construct_fc_layer(out_num_channels, out_num_channels))
+                self.part_cat_fc.append(self._construct_fc_layer(2 * out_num_channels, out_num_channels))
+
+        fc_layers, classifier_layers = [], []
+        for _ in range(self.num_parts + 1):  # main branch + part-based branches
+            fc_layers.append(self._construct_fc_layer(out_num_channels, self.feature_dim, dropout=False))
+            classifier_layers.append(classifier_block(self.feature_dim, real_data_num_classes))
+        self.fc = nn.ModuleList(fc_layers)
+        self.classifier = nn.ModuleList(classifier_layers)
+
+        self.aux_fc = None
+        self.aux_classifier = None
+        self.split_embeddings = synthetic_data_num_classes is not None
+        if self.split_embeddings:
+            aux_fc_layers, aux_classifier_layers = [], []
+            for _ in range(self.num_parts + 1):  # main branch + part-based branches
+                aux_fc_layers.append(self._construct_fc_layer(out_num_channels, self.feature_dim, dropout=False))
+                aux_classifier_layers.append(classifier_block(self.feature_dim, synthetic_data_num_classes))
+            self.aux_fc = nn.ModuleList(aux_fc_layers)
+            self.aux_classifier = nn.ModuleList(aux_classifier_layers)
+
+        self.attr_fc = None
+        self.attr_classifiers = None
+        if enable_attr_tasks and attr_tasks is not None and len(attr_tasks) > 0:
+            attr_fc = dict()
+            attr_classifier = dict()
+            for attr_name, attr_num_classes in attr_tasks.items():
+                attr_fc[attr_name] = self._construct_fc_layer(out_num_channels, self.feature_dim // 4, dropout=False)
+                attr_classifier[attr_name] = AngleSimpleLinear(self.feature_dim // 4, attr_num_classes)
+            self.attr_fc = nn.ModuleDict(attr_fc)
+            self.attr_classifiers = nn.ModuleDict(attr_classifier)
 
         self._init_params()
 
-    def _make_layer(
-        self,
-        block,
-        layer,
-        in_channels,
-        out_channels,
-        reduce_spatial_size,
-        IN=False
-    ):
-        layers = []
-
-        layers.append(block(in_channels, out_channels, IN=IN))
+    @staticmethod
+    def _make_layer(block, layer, in_channels, out_channels, reduce_spatial_size, IN=False):
+        layers = [block(in_channels, out_channels, IN=IN)]
         for i in range(1, layer):
             layers.append(block(out_channels, out_channels, IN=IN))
 
@@ -366,75 +416,141 @@ class OSNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def _construct_fc_layer(self, fc_dims, input_dim, dropout_p=None):
-        if fc_dims is None or fc_dims < 0:
-            self.feature_dim = input_dim
-            return None
-
-        if isinstance(fc_dims, int):
-            fc_dims = [fc_dims]
-
+    @staticmethod
+    def _construct_fc_layer(input_dim, out_dim, dropout=False):
         layers = []
-        for dim in fc_dims:
-            layers.append(nn.Linear(input_dim, dim))
-            layers.append(nn.BatchNorm1d(dim))
-            layers.append(nn.ReLU(inplace=True))
-            if dropout_p is not None:
-                layers.append(nn.Dropout(p=dropout_p))
-            input_dim = dim
 
-        self.feature_dim = fc_dims[-1]
+        if dropout:
+            layers.append(Dropout(p=0.5, dist='gaussian'))
+
+        layers.extend([
+            nn.Linear(input_dim, out_dim),
+            nn.BatchNorm1d(out_dim)
+        ])
 
         return nn.Sequential(*layers)
 
     def _init_params(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight, mode='fan_out', nonlinearity='relu'
-                )
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
-
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-
-            elif isinstance(m, nn.BatchNorm1d):
+            elif isinstance(m, (nn.InstanceNorm1d, nn.InstanceNorm2d)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0, 0.01)
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
 
-    def featuremaps(self, x):
-        x = self.conv1(x)
-        x = self.maxpool(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        return x
+    def _backbone(self, x):
+        y = self.conv1(x)
+        y = self.maxpool(y)
+        y = self.conv2(y)
+        y = self.conv3(y)
+        y = self.conv4(y)
+        y = self.conv5(y)
 
-    def forward(self, x, return_featuremaps=False):
-        x = self.featuremaps(x)
+        return y
+
+    def _glob_feature_vector(self, x, num_parts):
+        return F.adaptive_avg_pool2d(x, 1).view(x.size(0), -1)
+
+    def _part_feature_vector(self, x, num_parts):
+        if num_parts <= 1:
+            return []
+
+        gap_branch = F.adaptive_avg_pool2d(x, (num_parts, 1)).squeeze(dim=-1)
+        gmp_branch = F.adaptive_max_pool2d(x, (num_parts, 1)).squeeze(dim=-1)
+        feature_vectors = gap_branch + gmp_branch
+
+        return [f.squeeze(dim=-1) for f in torch.split(feature_vectors, 1, dim=-1)]
+
+    def forward(self, x, return_featuremaps=False, get_embeddings=False, return_logits=False):
+        feature_maps = self._backbone(x)
         if return_featuremaps:
-            return x
-        v = self.global_avgpool(x)
-        v = v.view(v.size(0), -1)
-        if self.fc is not None:
-            v = self.fc(v)
-        if not self.training:
-            return v
-        y = self.classifier(v)
-        if self.loss == 'softmax':
-            return y
-        elif self.loss == 'triplet':
-            return y, v
+            return feature_maps
+
+        glob_feature = self._glob_feature_vector(feature_maps, num_parts=self.num_parts)
+        part_features = self._part_feature_vector(feature_maps, num_parts=self.num_parts)
+        features = [glob_feature] + list(part_features)
+
+        main_embeddings = [fc(f) for f, fc in zip(features, self.fc)]
+        if not self.training and not return_logits:
+            return torch.cat(main_embeddings, dim=-1)
+
+        main_logits = [classifier(embd) for embd, classifier in zip(main_embeddings, self.classifier)]
+        main_centers = [classifier.get_centers() for classifier in self.classifier]
+
+        if self.split_embeddings:
+            aux_embeddings = [fc(f) for f, fc in zip(features, self.aux_fc)]
+            aux_logits = [classifier(embd) for embd, classifier in zip(aux_embeddings, self.aux_classifier)]
+            aux_centers = [classifier.get_centers() for classifier in self.aux_classifier]
+        else:
+            aux_embeddings = [None] * len(features)
+            aux_logits = [None] * len(features)
+            aux_centers = [None] * len(features)
+
+        all_embeddings = dict(real=main_embeddings, synthetic=aux_embeddings)
+        all_outputs = dict(real=main_logits, synthetic=aux_logits,
+                           real_centers=main_centers, synthetic_centers=aux_centers)
+
+        attr_embeddings = dict()
+        if self.attr_fc is not None:
+            for attr_name, attr_fc in self.attr_fc.items():
+                attr_embeddings[attr_name] = attr_fc(glob_feature)
+
+        attr_logits = dict()
+        if self.attr_classifiers is not None:
+            for att_name, attr_classifier in self.attr_classifiers.items():
+                attr_logits[att_name] = attr_classifier(attr_embeddings[att_name])
+
+        if get_embeddings:
+            return all_embeddings, all_outputs, attr_logits
+
+        if self.loss in ['softmax', 'am_softmax']:
+            return all_outputs, attr_logits
+        elif self.loss in ['triplet']:
+            return all_outputs, attr_logits, all_embeddings
         else:
             raise KeyError("Unsupported loss: {}".format(self.loss))
+
+    def load_pretrained_weights(self, pretrained_dict):
+        model_dict = self.state_dict()
+        new_state_dict = OrderedDict()
+        matched_layers, discarded_layers = [], []
+
+        for k, v in pretrained_dict.items():
+            if k.startswith('module.'):
+                k = k[7:]  # discard module.
+
+            if k in model_dict and model_dict[k].size() == v.size():
+                new_state_dict[k] = v
+                matched_layers.append(k)
+            else:
+                discarded_layers.append(k)
+
+        model_dict.update(new_state_dict)
+        self.load_state_dict(model_dict)
+
+        if len(matched_layers) == 0:
+            warnings.warn(
+                'The pretrained weights cannot be loaded, '
+                'please check the key names manually '
+                '(** ignored and continue **)'
+            )
+        else:
+            print('Successfully loaded pretrained weights')
+            if len(discarded_layers) > 0:
+                print(
+                    '** The following layers are discarded '
+                    'due to unmatched keys or layer size: {}'.
+                        format(discarded_layers)
+                )
 
 
 def init_pretrained_weights(model, key=''):
@@ -445,7 +561,6 @@ def init_pretrained_weights(model, key=''):
     import os
     import errno
     import gdown
-    from collections import OrderedDict
 
     def _get_torch_home():
         ENV_TORCH_HOME = 'TORCH_HOME'
@@ -479,108 +594,78 @@ def init_pretrained_weights(model, key=''):
         gdown.download(pretrained_urls[key], cached_file, quiet=False)
 
     state_dict = torch.load(cached_file)
-    model_dict = model.state_dict()
-    new_state_dict = OrderedDict()
-    matched_layers, discarded_layers = [], []
-
-    for k, v in state_dict.items():
-        if k.startswith('module.'):
-            k = k[7:] # discard module.
-
-        if k in model_dict and model_dict[k].size() == v.size():
-            new_state_dict[k] = v
-            matched_layers.append(k)
-        else:
-            discarded_layers.append(k)
-
-    model_dict.update(new_state_dict)
-    model.load_state_dict(model_dict)
-
-    if len(matched_layers) == 0:
-        warnings.warn(
-            'The pretrained weights from "{}" cannot be loaded, '
-            'please check the key names manually '
-            '(** ignored and continue **)'.format(cached_file)
-        )
-    else:
-        print(
-            'Successfully loaded imagenet pretrained weights from "{}"'.
-            format(cached_file)
-        )
-        if len(discarded_layers) > 0:
-            print(
-                '** The following layers are discarded '
-                'due to unmatched keys or layer size: {}'.
-                format(discarded_layers)
-            )
+    model.load_pretrained_weights(state_dict)
 
 
 ##########
 # Instantiation
 ##########
-def osnet_x1_0(num_classes=1000, pretrained=True, loss='softmax', **kwargs):
+
+def osnet_x1_0(num_classes, pretrained=False, download_weights=False, **kwargs):
     # standard size (width x1.0)
     model = OSNet(
         num_classes,
         blocks=[OSBlock, OSBlock, OSBlock],
         layers=[2, 2, 2],
         channels=[64, 256, 384, 512],
-        loss=loss,
         **kwargs
     )
-    if pretrained:
+
+    if pretrained and download_weights:
         init_pretrained_weights(model, key='osnet_x1_0')
+
     return model
 
 
-def osnet_x0_75(num_classes=1000, pretrained=True, loss='softmax', **kwargs):
+def osnet_x0_75(num_classes, pretrained=False, download_weights=False, **kwargs):
     # medium size (width x0.75)
     model = OSNet(
         num_classes,
         blocks=[OSBlock, OSBlock, OSBlock],
         layers=[2, 2, 2],
         channels=[48, 192, 288, 384],
-        loss=loss,
         **kwargs
     )
-    if pretrained:
+
+    if pretrained and download_weights:
         init_pretrained_weights(model, key='osnet_x0_75')
+
     return model
 
 
-def osnet_x0_5(num_classes=1000, pretrained=True, loss='softmax', **kwargs):
+def osnet_x0_5(num_classes, pretrained=False, download_weights=False, **kwargs):
     # tiny size (width x0.5)
     model = OSNet(
         num_classes,
         blocks=[OSBlock, OSBlock, OSBlock],
         layers=[2, 2, 2],
         channels=[32, 128, 192, 256],
-        loss=loss,
         **kwargs
     )
-    if pretrained:
+
+    if pretrained and download_weights:
         init_pretrained_weights(model, key='osnet_x0_5')
+
     return model
 
 
-def osnet_x0_25(num_classes=1000, pretrained=True, loss='softmax', **kwargs):
+def osnet_x0_25(num_classes, pretrained=False, download_weights=False, **kwargs):
     # very tiny size (width x0.25)
     model = OSNet(
         num_classes,
         blocks=[OSBlock, OSBlock, OSBlock],
         layers=[2, 2, 2],
         channels=[16, 64, 96, 128],
-        loss=loss,
         **kwargs
     )
-    if pretrained:
+
+    if pretrained and download_weights:
         init_pretrained_weights(model, key='osnet_x0_25')
+
     return model
 
 
-def osnet_ibn_x1_0(
-    num_classes=1000, pretrained=True, loss='softmax', **kwargs
-):
+def osnet_ibn_x1_0(num_classes, pretrained=False, download_weights=False, **kwargs):
     # standard size (width x1.0) + IBN layer
     # Ref: Pan et al. Two at Once: Enhancing Learning and Generalization Capacities via IBN-Net. ECCV, 2018.
     model = OSNet(
@@ -588,10 +673,11 @@ def osnet_ibn_x1_0(
         blocks=[OSBlock, OSBlock, OSBlock],
         layers=[2, 2, 2],
         channels=[64, 256, 384, 512],
-        loss=loss,
         IN=True,
         **kwargs
     )
-    if pretrained:
+
+    if pretrained and download_weights:
         init_pretrained_weights(model, key='osnet_ibn_x1_0')
+
     return model
