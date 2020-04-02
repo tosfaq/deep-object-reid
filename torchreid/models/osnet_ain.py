deed7bc6196bbd1fa2145f9b668101f50b5acca3
Evgeny Izutov <evgeny.izutov@intel.com>
2020-04-02 13:01:02 +0300

    Set to stable

--- torchreid/models/osnet_ain.py
+++ torchreid/models/osnet_ain.py
@@ -467,7 +467,7 @@ class OSNet(nn.Module):
 
         fc_layers, classifier_layers = [], []
         for _ in range(self.num_parts + 1):  # main branch + part-based branches
-            fc_layers.append(self._construct_ext_fc_layer(out_num_channels, self.feature_dim, dropout=False))
+            fc_layers.append(self._construct_fc_layer(out_num_channels, self.feature_dim, dropout=False))
             classifier_layers.append(classifier_block(self.feature_dim, real_data_num_classes))
         self.fc = nn.ModuleList(fc_layers)
         self.classifier = nn.ModuleList(classifier_layers)
@@ -478,7 +478,7 @@ class OSNet(nn.Module):
         if self.split_embeddings:
             aux_fc_layers, aux_classifier_layers = [], []
             for _ in range(self.num_parts + 1):  # main branch + part-based branches
-                aux_fc_layers.append(self._construct_ext_fc_layer(out_num_channels, self.feature_dim, dropout=False))
+                aux_fc_layers.append(self._construct_fc_layer(out_num_channels, self.feature_dim, dropout=False))
                 aux_classifier_layers.append(classifier_block(self.feature_dim, synthetic_data_num_classes))
             self.aux_fc = nn.ModuleList(aux_fc_layers)
             self.aux_classifier = nn.ModuleList(aux_classifier_layers)
