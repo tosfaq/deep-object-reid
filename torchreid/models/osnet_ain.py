8285db90b495b1fe006a622219f00c443d511975
Evgeny Izutov <evgeny.izutov@intel.com>
2020-04-29 15:06:49 +0300

    Set to default

--- torchreid/models/osnet_ain.py
+++ torchreid/models/osnet_ain.py
@@ -724,8 +724,8 @@ def osnet_ain_x1_0(num_classes, pretrained=False, download_weights=False, enable
         channels=[64, 256, 384, 512],
         # head_attention=enable_attentions,
         attentions=[False, True, True, False, False] if enable_attentions else None,
-        input_lcn=True,
-        input_IN=False,
+        input_lcn=False,
+        input_IN=True,
         conv1_IN=True,
         **kwargs
     )
