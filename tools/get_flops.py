8330da7584b39a56c2c9eff0ad4d5edc39d175ce
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2020-11-26 11:40:28 +0300

    Add a tool to count flops

---
+++ tools/get_flops.py
@@ -0,0 +1,63 @@
+import argparse
+import json
+
+import torch
+
+import torchreid
+from torchreid.utils import (
+    set_random_seed, collect_env_info, compute_model_complexity
+)
+
+from scripts.default_config import (
+    model_kwargs, get_default_config, imagedata_kwargs
+)
+
+
+def build_datamanager(cfg):
+    if cfg.data.type == 'image':
+        return torchreid.data.ImageDataManager(**imagedata_kwargs(cfg))
+    else:
+        return torchreid.data.VideoDataManager(**videodata_kwargs(cfg))
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--config-file', type=str, default='',
+                        help='path to config file')
+    parser.add_argument('--out')
+    parser.add_argument('opts', default=None, nargs=argparse.REMAINDER,
+                        help='Modify config options using the command-line')
+    args = parser.parse_args()
+
+    cfg = get_default_config()
+    cfg.use_gpu = torch.cuda.is_available()
+    if args.config_file:
+        cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    set_random_seed(cfg.train.seed)
+
+    print('Show configuration\n{}\n'.format(cfg))
+    print('Collecting env info ...')
+    print('** System info **\n{}\n'.format(collect_env_info()))
+
+    if cfg.use_gpu:
+        torch.backends.cudnn.benchmark = True
+
+    datamanager = build_datamanager(cfg)
+    num_train_classes = datamanager.num_train_pids
+
+    print('Building main model: {}'.format(cfg.model.name))
+    model = torchreid.models.build_model(**model_kwargs(cfg, num_train_classes))
+    num_params, flops = compute_model_complexity(model, (1, 3, cfg.data.height, cfg.data.width))
+    print('Main model complexity: params={:,} flops={:,}'.format(num_params, flops * 2))
+
+    if args.out:
+        out = list()
+        out.append({'key': 'size', 'display_name': 'Size', 'value': num_params / 10**6, 'unit': 'Mp'})
+        out.append({'key': 'complexity', 'display_name': 'Complexity', 'value': 2 * flops / 10**9,
+                    'unit': 'GFLOPs'})
+        print('dump to' + args.out)
+        with open(args.out, 'w') as write_file:
+            json.dump(out, write_file, indent=4)
+
+if __name__ == '__main__':
+    main()
