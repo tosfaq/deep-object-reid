32e757b4bfe50bbceb4563b2a52fbe0da219419e
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2021-03-29 22:24:43 +0300

    Add monitors to engine

--- torchreid/engine/engine.py
+++ torchreid/engine/engine.py
@@ -186,7 +186,9 @@ class Engine:
         use_metric_cuhk03=False,
         ranks=(1, 5, 10, 20),
         rerank=False,
-        lr_finder = False,
+        lr_finder=False,
+        perf_monitor=None,
+        stop_callback=None,
         **kwargs
     ):
         r"""A unified pipeline for training and evaluating a model.
@@ -245,14 +247,18 @@ class Engine:
         self.fixbase_epoch = fixbase_epoch
         print('=> Start training')
 
+        if perf_monitor and not lr_finder: perf_monitor.on_train_begin()
         for self.epoch in range(self.start_epoch, self.max_epoch):
-
+            if perf_monitor and not lr_finder: perf_monitor.on_train_epoch_begin()
             self.train(
                 print_freq=print_freq,
                 fixbase_epoch=fixbase_epoch,
                 open_layers=open_layers,
-                lr_finder = lr_finder
+                lr_finder = lr_finder,
+                perf_monitor=perf_monitor,
+                stop_callback=stop_callback
             )
+            if perf_monitor and not lr_finder: perf_monitor.on_train_epoch_end()
 
             if ((self.epoch + 1) >= start_eval
                and eval_freq > 0
@@ -299,6 +305,8 @@ class Engine:
             if lr_finder:
                 print(f"epoch: {self.epoch}\t top1: {top1_final}\t lr: {self.get_current_lr()}")
 
+        if perf_monitor and not lr_finder: perf_monitor.on_train_end()
+
         elapsed = round(time.time() - time_start)
         elapsed = str(datetime.timedelta(seconds=elapsed))
         print('Elapsed {}'.format(elapsed))
@@ -308,7 +316,8 @@ class Engine:
 
         return top1_final
 
-    def train(self, print_freq=10, fixbase_epoch=0, open_layers=None, lr_finder=False):
+    def train(self, print_freq=10, fixbase_epoch=0, open_layers=None, lr_finder=False, perf_monitor=None,
+              stop_callback=None):
         losses = MetricMeter()
         batch_time = AverageMeter()
         data_time = AverageMeter()
@@ -323,6 +332,8 @@ class Engine:
         self.num_batches = len(self.train_loader)
         end = time.time()
         for self.batch_idx, data in enumerate(self.train_loader):
+            if perf_monitor and not lr_finder: perf_monitor.on_train_batch_begin()
+
             data_time.update(time.time() - end)
 
             loss_summary, avg_acc = self.forward_backward(data)
@@ -330,6 +341,7 @@ class Engine:
 
             losses.update(loss_summary)
             accuracy.update(avg_acc)
+            if perf_monitor and not lr_finder: perf_monitor.on_train_batch_end()
 
             if not lr_finder and ((self.batch_idx + 1) % print_freq) == 0:
                 nb_this_epoch = self.num_batches - (self.batch_idx + 1)
@@ -367,6 +379,8 @@ class Engine:
                     self.writer.add_scalar('Loss/' + name, meter.avg, n_iter)
 
             end = time.time()
+            if stop_callback and stop_callback.stop():
+                break
 
         if not lr_finder:
             self.update_lr(output_avg_metric = losses.meters['loss'].avg)
