c1b494fdfd21281b6ce05ac1857e26c19189f4f0
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-08-09 12:53:11 +0300

    Resolve comments

--- torchreid/engine/builder.py
+++ torchreid/engine/builder.py
@@ -1,7 +1,51 @@
+from typing import Dict
+
 from torchreid.engine import (ImageAMSoftmaxEngine, ImageContrastiveEngine,
                               ImageTripletEngine)
 from torchreid.integration.nncf.engine import AccuracyAwareImageAMSoftmaxEngine
 from torchreid.integration.nncf.utils import extract_accuracy_aware_training_config
+
+
+def get_params_for_image_am_softmax_engine(cfg) -> Dict[str, object]:
+    image_am_softmax_engine_params = {
+        'reg_cfg': cfg.reg,
+        'metric_cfg': cfg.metric_losses,
+        'use_gpu': cfg.use_gpu,
+        'save_chkpt': cfg.model.save_chkpt,
+        'train_patience': cfg.train.train_patience,
+        'early_stoping': cfg.train.early_stoping,
+        'lr_decay_factor': cfg.train.lr_decay_factor,
+        'conf_penalty': cfg.loss.softmax.conf_penalty,
+        'label_smooth': cfg.loss.softmax.label_smooth,
+        'aug_type': cfg.loss.softmax.augmentations.aug_type,
+        'aug_prob': cfg.loss.softmax.augmentations.aug_prob,
+        'decay_power': cfg.loss.softmax.augmentations.fmix.decay_power,
+        'alpha': cfg.loss.softmax.augmentations.alpha,
+        'size': (cfg.data.height, cfg.data.width),
+        'max_soft': cfg.loss.softmax.augmentations.fmix.max_soft,
+        'reformulate': cfg.loss.softmax.augmentations.fmix.reformulate,
+        'pr_product': cfg.loss.softmax.pr_product,
+        'm': cfg.loss.softmax.m,
+        's': cfg.loss.softmax.s,
+        'compute_s': cfg.loss.softmax.compute_s,
+        'margin_type': cfg.loss.softmax.margin_type,
+        'end_s': cfg.loss.softmax.end_s,
+        'duration_s': cfg.loss.softmax.duration_s,
+        'skip_steps_s': cfg.loss.softmax.skip_steps_s,
+        'enable_masks': cfg.data.enable_masks,
+        'adaptive_margins': cfg.loss.softmax.adaptive_margins,
+        'class_weighting': cfg.loss.softmax.class_weighting,
+        'attr_cfg': cfg.attr_loss,
+        'base_num_classes': cfg.loss.softmax.base_num_classes,
+        'symmetric_ce': cfg.loss.softmax.symmetric_ce,
+        'mix_weight': cfg.mixing_loss.enable * cfg.mixing_loss.weight,
+        'enable_rsc': cfg.model.self_challenging_cfg.enable,
+        'enable_sam': cfg.sam.enable,
+        'use_ema_decay': cfg.train.ema.enable,
+        'ema_decay': cfg.train.ema.ema_decay
+    }
+    return image_am_softmax_engine_params
+
 
 def build_engine(cfg, datamanager, model, optimizer, scheduler,
                  should_freeze_aux_models=False,
@@ -15,100 +59,34 @@ def build_engine(cfg, datamanager, model, optimizer, scheduler,
     is_accuracy_aware_training = extract_accuracy_aware_training_config(nncf_metainfo.nncf_config)
     initial_lr = initial_lr if initial_lr else cfg.train.lr
     if cfg.loss.name in ['softmax', 'am_softmax']:
+        image_am_softmax_engine_params = get_params_for_image_am_softmax_engine(cfg)
         softmax_type = 'stock' if cfg.loss.name == 'softmax' else 'am'
         if not is_accuracy_aware_training:
             engine = ImageAMSoftmaxEngine(
                 datamanager,
                 models=model,
                 optimizers=optimizer,
-                reg_cfg=cfg.reg,
-                metric_cfg=cfg.metric_losses,
                 schedulers=scheduler,
-                use_gpu=cfg.use_gpu,
-                save_chkpt=cfg.model.save_chkpt,
-                train_patience=cfg.train.train_patience,
-                early_stoping=cfg.train.early_stoping,
-                lr_decay_factor=cfg.train.lr_decay_factor,
-                conf_penalty=cfg.loss.softmax.conf_penalty,
-                label_smooth=cfg.loss.softmax.label_smooth,
-                aug_type=cfg.loss.softmax.augmentations.aug_type,
-                aug_prob=cfg.loss.softmax.augmentations.aug_prob,
-                decay_power=cfg.loss.softmax.augmentations.fmix.decay_power,
-                alpha=cfg.loss.softmax.augmentations.alpha,
-                size=(cfg.data.height, cfg.data.width),
-                max_soft=cfg.loss.softmax.augmentations.fmix.max_soft,
-                reformulate=cfg.loss.softmax.augmentations.fmix.reformulate,
-                pr_product=cfg.loss.softmax.pr_product,
                 softmax_type=softmax_type,
-                m=cfg.loss.softmax.m,
-                s=cfg.loss.softmax.s,
-                compute_s=cfg.loss.softmax.compute_s,
-                margin_type=cfg.loss.softmax.margin_type,
-                end_s=cfg.loss.softmax.end_s,
-                duration_s=cfg.loss.softmax.duration_s,
-                skip_steps_s=cfg.loss.softmax.skip_steps_s,
-                enable_masks=cfg.data.enable_masks,
-                adaptive_margins=cfg.loss.softmax.adaptive_margins,
-                class_weighting=cfg.loss.softmax.class_weighting,
-                attr_cfg=cfg.attr_loss,
-                base_num_classes=cfg.loss.softmax.base_num_classes,
-                symmetric_ce=cfg.loss.softmax.symmetric_ce,
-                mix_weight=cfg.mixing_loss.enable * cfg.mixing_loss.weight,
-                enable_rsc=cfg.model.self_challenging_cfg.enable,
-                enable_sam=cfg.sam.enable,
                 should_freeze_aux_models=should_freeze_aux_models,
                 nncf_metainfo=nncf_metainfo,
                 initial_lr=initial_lr,
-                use_ema_decay=cfg.train.ema.enable,
-                ema_decay=cfg.train.ema.ema_decay
+                **image_am_softmax_engine_params
             )
         else:
+            # TODO:(kshpv) lets take *target_metric_name* from config?
             target_metric_name = 'top1'
             engine = AccuracyAwareImageAMSoftmaxEngine(
                 datamanager,
                 models=model,
                 optimizers=optimizer,
-                reg_cfg=cfg.reg,
-                metric_cfg=cfg.metric_losses,
                 schedulers=scheduler,
-                use_gpu=cfg.use_gpu,
-                save_chkpt=cfg.model.save_chkpt,
-                train_patience=cfg.train.train_patience,
-                early_stoping=cfg.train.early_stoping,
-                lr_decay_factor=cfg.train.lr_decay_factor,
-                conf_penalty=cfg.loss.softmax.conf_penalty,
-                label_smooth=cfg.loss.softmax.label_smooth,
-                aug_type=cfg.loss.softmax.augmentations.aug_type,
-                aug_prob=cfg.loss.softmax.augmentations.aug_prob,
-                decay_power=cfg.loss.softmax.augmentations.fmix.decay_power,
-                alpha=cfg.loss.softmax.augmentations.alpha,
-                size=(cfg.data.height, cfg.data.width),
-                max_soft=cfg.loss.softmax.augmentations.fmix.max_soft,
-                reformulate=cfg.loss.softmax.augmentations.fmix.reformulate,
-                pr_product=cfg.loss.softmax.pr_product,
                 softmax_type=softmax_type,
-                m=cfg.loss.softmax.m,
-                s=cfg.loss.softmax.s,
-                compute_s=cfg.loss.softmax.compute_s,
-                margin_type=cfg.loss.softmax.margin_type,
-                end_s=cfg.loss.softmax.end_s,
-                duration_s=cfg.loss.softmax.duration_s,
-                skip_steps_s=cfg.loss.softmax.skip_steps_s,
-                enable_masks=cfg.data.enable_masks,
-                adaptive_margins=cfg.loss.softmax.adaptive_margins,
-                class_weighting=cfg.loss.softmax.class_weighting,
-                attr_cfg=cfg.attr_loss,
-                base_num_classes=cfg.loss.softmax.base_num_classes,
-                symmetric_ce=cfg.loss.softmax.symmetric_ce,
-                mix_weight=cfg.mixing_loss.enable * cfg.mixing_loss.weight,
-                enable_rsc=cfg.model.self_challenging_cfg.enable,
-                enable_sam=cfg.sam.enable,
                 should_freeze_aux_models=should_freeze_aux_models,
                 nncf_metainfo=nncf_metainfo,
                 initial_lr=initial_lr,
-                use_ema_decay=cfg.train.ema.enable,
-                ema_decay=cfg.train.ema.ema_decay,
-                target_metric_name=target_metric_name
+                target_metric_name=target_metric_name,
+                **image_am_softmax_engine_params
             )
     elif cfg.loss.name == 'contrastive':
         engine = ImageContrastiveEngine(
--- torchreid/integration/nncf/engine.py
+++ torchreid/integration/nncf/engine.py
@@ -2,43 +2,17 @@ from torchreid.engine.image import ImageAMSoftmaxEngine
 
 
 class AccuracyAwareImageAMSoftmaxEngine(ImageAMSoftmaxEngine):
-    def __init__(self, datamanager, models, optimizers, reg_cfg, metric_cfg, schedulers=None, use_gpu=False, save_chkpt=True,
-                 train_patience=10, early_stoping = False, lr_decay_factor = 1000, softmax_type='stock', label_smooth=False,
-                 margin_type='cos', epsilon=0.1, aug_type=None, decay_power=3, alpha=1., size=(224, 224), max_soft=0.0,
-                 reformulate=False, aug_prob=1., conf_penalty=False, pr_product=False, m=0.35, s=10, compute_s=False, end_s=None,
-                 duration_s=None, skip_steps_s=None, enable_masks=False, adaptive_margins=False, class_weighting=False,
-                 attr_cfg=None, base_num_classes=-1, symmetric_ce=False, mix_weight=1.0, enable_rsc=False, enable_sam=False,
-                 should_freeze_aux_models=False, nncf_metainfo=None, initial_lr=None, use_ema_decay=False, ema_decay=0.999, target_metric_name='top1'):
-        super().__init__(datamanager, models, optimizers, reg_cfg, metric_cfg, schedulers, use_gpu, save_chkpt,
-                         train_patience, early_stoping, lr_decay_factor, softmax_type, label_smooth,
-                         margin_type, epsilon, aug_type, decay_power, alpha, size, max_soft,
-                         reformulate, aug_prob, conf_penalty, pr_product, m, s, compute_s, end_s,
-                         duration_s, skip_steps_s, enable_masks, adaptive_margins, class_weighting,
-                         attr_cfg, base_num_classes, symmetric_ce, mix_weight, enable_rsc, enable_sam,
-                         should_freeze_aux_models, nncf_metainfo, initial_lr, use_ema_decay, ema_decay)
-
+    def __init__(self, datamanager, models, optimizers, reg_cfg, metric_cfg, *args, target_metric_name='top1', **kwargs):
+        super().__init__(datamanager, models, optimizers, reg_cfg, metric_cfg, *args, **kwargs)
         self.target_metric_name = target_metric_name
 
     def run(self, compression_ctrl, nncf_config, **kwargs):
         from nncf.common.accuracy_aware_training import create_accuracy_aware_training_loop
 
-        def is_mutual_learning_enable():
-            if len(self.models) > 1:
-                return True
-            elif len(self.models) == 1:
-                return False
-            else:
-                raise RuntimeError('Incorrect number of models')
-
-        if not is_mutual_learning_enable():
-            model = self.models['model']
-            optimizer = self.optims['model']
-            scheduler = self.scheds['model']
-        else:
-            # Use only main model
-            model = self.models['model_0']
-            optimizer = self.optims['model_0']
-            scheduler = self.scheds['model_0']
+        # Use only main model
+        model = self.models[self.main_model_name]
+        optimizer = self.optims[self.main_model_name]
+        scheduler = self.scheds[self.main_model_name]
 
         def _configure_optimizers_fn():
             return optimizer, scheduler
