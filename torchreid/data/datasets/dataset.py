Fix multilabel mAP calculation

torchreid/data/datasets/dataset.py
@@ -54,6 +54,8 @@ class Dataset:
 
         self.num_train_pids = self.get_num_pids(self.train)
         self.num_train_cams = self.get_num_cams(self.train)
+        if len(self.num_train_pids) == 0: # workaround for classification: query is a validation set
+            self.num_train_pids = self.get_num_pids(self.query)
 
         if self.combineall:
             self.combine_all()
@@ -135,12 +137,12 @@ class Dataset:
         Args:
             data (list): contains tuples of (img_path(s), pid, camid)
         """
-
         pids, cams = defaultdict(set), defaultdict(set)
         for record in data:
             dataset_id = record[3] if len(record) > 3 else 0
             if isinstance(record[1], (tuple, list)):
-                [pids[dataset_id].add(ids) for ids in record[1]]
+                for ids in record[1]:
+                    pids[dataset_id].add(ids)
             else:
                 pids[dataset_id].add(record[1])
             cams[dataset_id].add(record[2])
@@ -369,7 +371,7 @@ class ImageDataset(Dataset):
 
         if len(input_record) > 3:
             dataset_id = input_record[3]
-            if isinstance(obj_id, (tuple, list)): # than multi-label classification is available
+            if isinstance(obj_id, (tuple, list)): # when multi-label classification is available
                 targets = torch.zeros(self.num_train_pids[dataset_id])
                 for obj in obj_id:
                     targets[obj] = 1

torchreid/engine/engine.py
@@ -627,7 +627,7 @@ class Engine:
                         model_name=model_name,
                         dataset_name=dataset_name,
                         ranks=ranks,
-                        lr_finder = lr_finder
+                        lr_finder=lr_finder
                     )
                     if self.use_ema_decay and not lr_finder and not test_only:
                         ema_top1, ema_top5, ema_mAP = self._evaluate_classification(
@@ -639,8 +639,18 @@ class Engine:
                             ranks=ranks,
                             lr_finder = lr_finder
                         )
-                elif model_type == 'contrastive' or model_type == 'multilabel':
+                elif model_type == 'contrastive':
                     pass
+                elif model_type == 'multilabel':
+                    cur_mAP = self._evaluate_multilabel_classification(
+                        model=model,
+                        epoch=epoch,
+                        data_loader=self.test_loader[dataset_name]['query'],
+                        model_name=model_name,
+                        dataset_name=dataset_name,
+                        lr_finder=lr_finder
+                    )
+                    cur_top1, cur_top5 = (cur_mAP, cur_mAP)
                 elif dataset_name == 'lfw':
                     self._evaluate_pairwise(
                         model=model,
@@ -673,6 +683,20 @@ class Engine:
                     top5 =  max(cur_top5, ema_top5) if self.use_ema_decay else cur_top5
                     mAP =  max(cur_mAP, ema_mAP) if self.use_ema_decay else cur_mAP
         return top1, top5, mAP
+
+    torch.no_grad()
+    def _evaluate_multilabel_classification(self, model, epoch, data_loader, model_name, dataset_name, lr_finder):
+
+        mAP = metrics.evaluate_multilabel_classification(data_loader, model, self.use_gpu)
+
+        if self.writer is not None and not lr_finder:
+            self.writer.add_scalar('Val/{}/{}/mAP'.format(dataset_name, model_name), mAP, epoch + 1)
+
+        if not lr_finder:
+            print('** Results ({}) **'.format(model_name))
+            print('mAP: {:.2%}'.format(mAP))
+
+        return mAP
 
     @torch.no_grad()
     def _evaluate_classification(self, model, epoch, data_loader, model_name, dataset_name, ranks, lr_finder):

torchreid/metrics/classification.py
@@ -19,10 +19,10 @@ def score_extraction(data_loader, model, use_gpu, labelmap=[], head_id=0):
                     batch_labels[torch.where(batch_labels==i)] = label
 
             out_scores.append(model(batch_images)[head_id])
-            gt_labels.extend(batch_labels)
+            gt_labels.append(batch_labels)
 
         out_scores = torch.cat(out_scores, 0).data.cpu().numpy()
-        gt_labels = np.asarray(gt_labels)
+        gt_labels = torch.cat(gt_labels, 0).data.cpu().numpy()
 
     return out_scores, gt_labels
 
@@ -157,20 +157,50 @@ def evaluate_classification(dataloader, model, use_gpu, topk=(1,), labelmap=[]):
 
 
 def evaluate_multilabel_classification(dataloader, model, use_gpu):
+
+    def average_precision(output, target):
+        epsilon = 1e-8
+
+        # sort examples
+        indices = output.argsort()[::-1]
+        # Computes prec@i
+        total_count_ = np.cumsum(np.ones((len(output), 1)))
+
+        target_ = target[indices]
+        ind = target_ == 1
+        pos_count_ = np.cumsum(ind)
+        total = pos_count_[-1]
+        pos_count_[np.logical_not(ind)] = 0
+        pp = pos_count_ / total_count_
+        precision_at_i_ = np.sum(pp)
+        precision_at_i = precision_at_i_ / (total + epsilon)
+
+        return precision_at_i
+
+
+    def mAP(targs, preds):
+        """Returns the model's average precision for each class
+        Return:
+            ap (FloatTensor): 1xK tensor, with avg precision for each class k
+        """
+
+        if np.size(preds) == 0:
+            return 0
+        ap = np.zeros((preds.shape[1]))
+        # compute average precision for each class
+        for k in range(preds.shape[1]):
+            # sort scores
+            scores = preds[:, k]
+            targets = targs[:, k]
+            # compute average precision
+            ap[k] = average_precision(scores, targets)
+        return 100 * ap.mean()
+
     if isinstance(model, torch.nn.Module):
-        scores, labels = score_extraction(dataloader, model, use_gpu, labelmap)
+        scores, labels = score_extraction(dataloader, model, use_gpu)
     else:
-        scores, labels = score_extraction_from_ir(dataloader, model, labelmap)
-    print(scores.shape)
-    print(labels.shape)
+        scores, labels = score_extraction_from_ir(dataloader, model)
 
-    '''
-    m_ap = mean_average_precision(scores, labels)
+    mAP_score = mAP(1. / (1 + np.exp(-scores)), labels)
 
-    cmc = []
-    for k in topk:
-        cmc.append(mean_top_k_accuracy(scores, labels, k=k))
-
-    norm_cm = norm_confusion_matrix(scores, labels)
-    '''
-    #return cmc, m_ap, norm_cm
+    return mAP_score