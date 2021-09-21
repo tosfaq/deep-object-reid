0905aaa57fdedfd33306cc67ac67474c0fc82a83
kirill <kirill.prokofiev@intel.com>
2021-09-21 14:41:09 +0300

    added asam

--- torchreid/optim/sam.py
+++ torchreid/optim/sam.py
@@ -18,27 +18,27 @@ import torch
 
 
 class SAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+    def __init__(self, params, base_optimizer, rho=0.05, adaptive=True, **kwargs):
         assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
-        self.rho = rho
         self.base_optimizer = base_optimizer
-        defaults = dict(rho=rho, **self.base_optimizer.defaults)
-        super().__init__(params, defaults)
+        defaults = dict(rho=rho, adaptive=adaptive, **self.base_optimizer.defaults)
 
+        super().__init__(params, defaults)
+        self.rho = rho
+        self.adaptive = adaptive
         self.param_groups = self.base_optimizer.param_groups
 
     @torch.no_grad()
     def first_step(self, zero_grad=False):
         grad_norm = self._grad_norm()
-
         for group in self.param_groups:
             scale = self.rho / (grad_norm + 1e-12)
 
             for p in group["params"]:
                 if p.grad is None: continue
-                e_w = p.grad * scale
+                self.state[p]["old_p"] = p.data.clone()
+                e_w = (torch.pow(p, 2) if self.adaptive else 1.0) * p.grad * scale.to(p)
                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
-                self.state[p]["e_w"] = e_w
 
         if zero_grad: self.zero_grad()
 
@@ -47,22 +47,78 @@ class SAM(torch.optim.Optimizer):
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None: continue
-                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
 
         self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
         if zero_grad: self.zero_grad()
 
+    @torch.no_grad()
     def step(self, closure=None):
-        raise NotImplementedError("SAM doesn't work like the other optimizers,"
-                                   " you should first call `first_step` and the `second_step`;")
+        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
 
     def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
         norm = torch.norm(
                     torch.stack([
-                        p.grad.norm(p=2)
+                        ((torch.abs(p) if self.adaptive else 1.0) * p.grad).norm(p=2).to(shared_device)
                         for group in self.param_groups for p in group["params"]
                         if p.grad is not None
                     ]),
                     p=2
                )
         return norm
+# class SAM(torch.optim.Optimizer):
+#     def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+#         assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+#         self.rho = rho
+#         self.base_optimizer = base_optimizer
+#         defaults = dict(rho=rho, **self.base_optimizer.defaults)
+#         super().__init__(params, defaults)
+
+#         self.param_groups = self.base_optimizer.param_groups
+
+#     @torch.no_grad()
+#     def first_step(self, zero_grad=False):
+#         grad_norm = self._grad_norm()
+
+#         for group in self.param_groups:
+#             scale = self.rho / (grad_norm + 1e-12)
+
+#             for p in group["params"]:
+#                 if p.grad is None: continue
+#                 e_w = p.grad * scale
+#                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
+#                 self.state[p]["e_w"] = e_w
+
+#         if zero_grad: self.zero_grad()
+
+#     @torch.no_grad()
+#     def second_step(self, zero_grad=False):
+#         for group in self.param_groups:
+#             for p in group["params"]:
+#                 if p.grad is None: continue
+#                 p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+
+#         self.base_optimizer.step()  # do the actual "sharpness-aware" update
+#         if zero_grad: self.zero_grad()
+
+#     def step(self, closure=None):
+#         raise NotImplementedError("SAM doesn't work like the other optimizers,"
+#                                    " you should first call `first_step` and the `second_step`;")
+
+#     def _grad_norm(self):
+#         norm = torch.norm(
+#                     torch.stack([
+#                         p.grad.norm(p=2)
+#                         for group in self.param_groups for p in group["params"]
+#                         if p.grad is not None
+#                     ]),
+#                     p=2
+#                )
+#         return norm
