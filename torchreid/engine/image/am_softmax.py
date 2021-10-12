fix mix precision with EMA and DP

torchreid/engine/image/am_softmax.py
@@ -90,8 +90,8 @@ class ImageAMSoftmaxEngine(Engine):
         self.max_soft = max_soft
         self.reformulate = reformulate
         self.prev_smooth_metric = 0.
+        self.mix_precision = mix_precision
         self.scaler = GradScaler(enabled=mix_precision)
-        self.forward_backward = autocast(mix_precision)(self.forward_backward)
 
         num_classes = self.datamanager.num_train_pids
         if not isinstance(num_classes, (list, tuple)):
@@ -200,114 +200,115 @@ class ImageAMSoftmaxEngine(Engine):
         return float(max(np.sqrt(2) * np.log(num_class - 1), 3))
 
     def forward_backward(self, data):
-        n_iter = self.epoch * self.num_batches + self.batch_idx
+        with autocast(enabled=self.mix_precision):
+            n_iter = self.epoch * self.num_batches + self.batch_idx
 
-        train_records = self.parse_data_for_train(data, True, self.enable_masks, self.use_gpu)
-        imgs = train_records['img']
-        obj_ids = train_records['obj_id']
-        num_packages = 1
-        if len(imgs.size()) != 4:
-            assert len(imgs.size()) == 5
+            train_records = self.parse_data_for_train(data, True, self.enable_masks, self.use_gpu)
+            imgs = train_records['img']
+            obj_ids = train_records['obj_id']
+            num_packages = 1
+            if len(imgs.size()) != 4:
+                assert len(imgs.size()) == 5
 
-            b, num_packages, c, h, w = imgs.size()
-            imgs = imgs.view(b * num_packages, c, h, w)
-            obj_ids = obj_ids.view(-1, 1).repeat(1, num_packages).view(-1)
-            train_records['dataset_id'] = train_records['dataset_id'].view(-1, 1).repeat(1, num_packages).view(-1)
+                b, num_packages, c, h, w = imgs.size()
+                imgs = imgs.view(b * num_packages, c, h, w)
+                obj_ids = obj_ids.view(-1, 1).repeat(1, num_packages).view(-1)
+                train_records['dataset_id'] = train_records['dataset_id'].view(-1, 1).repeat(1, num_packages).view(-1)
 
-        imgs, obj_ids = self._apply_batch_augmentation(imgs, obj_ids)
+            imgs, obj_ids = self._apply_batch_augmentation(imgs, obj_ids)
 
-        model_names = self.get_model_names()
-        num_models = len(model_names)
-        steps = [1, 2] if self.enable_sam and not self.lr_finder else [1]
-        for step in steps:
-            # if sam is enabled then statistics will be written each step, but will be saved only the second time
-            # this is made just for convinience
-            avg_acc = 0.0
-            out_logits = [[] for _ in range(self.num_targets)]
-            total_loss = torch.zeros([], dtype=imgs.dtype, device=imgs.device)
-            loss_summary = dict()
+            model_names = self.get_model_names()
+            num_models = len(model_names)
+            steps = [1, 2] if self.enable_sam and not self.lr_finder else [1]
+            for step in steps:
+                # if sam is enabled then statistics will be written each step, but will be saved only the second time
+                # this is made just for convinience
+                avg_acc = 0.0
+                out_logits = [[] for _ in range(self.num_targets)]
+                total_loss = torch.zeros([], dtype=imgs.dtype, device=imgs.device)
+                loss_summary = dict()
 
-            for model_name in model_names:
-                self.optims[model_name].zero_grad()
+                for model_name in model_names:
+                    self.optims[model_name].zero_grad()
 
-                model_loss, model_loss_summary, model_avg_acc, model_logits = self._single_model_losses(
-                    self.models[model_name], train_records, imgs, obj_ids, n_iter, model_name, num_packages
-                )
+                    model_loss, model_loss_summary, model_avg_acc, model_logits = self._single_model_losses(
+                        self.models[model_name], train_records, imgs, obj_ids, n_iter, model_name, num_packages
+                    )
 
-                avg_acc += model_avg_acc / float(num_models)
-                total_loss += model_loss / float(num_models)
-                loss_summary.update(model_loss_summary)
+                    avg_acc += model_avg_acc / float(num_models)
+                    total_loss += model_loss / float(num_models)
+                    loss_summary.update(model_loss_summary)
 
-                for trg_id in range(self.num_targets):
-                    if model_logits[trg_id] is not None:
-                        out_logits[trg_id].append(model_logits[trg_id])
+                    for trg_id in range(self.num_targets):
+                        if model_logits[trg_id] is not None:
+                            out_logits[trg_id].append(model_logits[trg_id])
 
-            if len(model_names) > 1:
-                num_mutual_losses = 0
-                mutual_loss = torch.zeros([], dtype=imgs.dtype, device=imgs.device)
-                for trg_id in range(self.num_targets):
-                    if len(out_logits[trg_id]) <= 1:
+                if len(model_names) > 1:
+                    num_mutual_losses = 0
+                    mutual_loss = torch.zeros([], dtype=imgs.dtype, device=imgs.device)
+                    for trg_id in range(self.num_targets):
+                        if len(out_logits[trg_id]) <= 1:
+                            continue
+
+                        with torch.no_grad():
+                            trg_probs = torch.softmax(torch.stack(out_logits[trg_id]), dim=2).mean(dim=0)
+
+                        for model_id, logits in enumerate(out_logits[trg_id]):
+                            log_probs = torch.log_softmax(logits, dim=1)
+                            m_loss = (trg_probs * log_probs).sum(dim=1).mean().neg()
+
+                            mutual_loss += m_loss
+                            loss_summary['mutual_{}/{}'.format(trg_id, model_names[model_id])] = m_loss.item()
+                            num_mutual_losses += 1
+
+                    should_turn_off_mutual_learning = self._should_turn_off_mutual_learning(self.epoch)
+                    coeff_mutual_learning = int(not should_turn_off_mutual_learning)
+
+                    total_loss += coeff_mutual_learning * mutual_loss / float(num_mutual_losses)
+                    if self.compression_ctrl:
+                        compression_loss = self.compression_ctrl.loss()
+                        loss_summary['compression_loss'] = compression_loss
+                        total_loss += compression_loss
+
+                # backward pass
+                self.scaler.scale(total_loss).backward(retain_graph=self.enable_metric_losses)
+
+                for model_name in model_names:
+                    if not self.models[model_name].training:
                         continue
+                    if self.clip_grad != 0 and step == 1:
+                        self.scaler.unscale_(self.optims[model_name])
+                        torch.nn.utils.clip_grad_norm_(self.models[model_name].parameters(), self.clip_grad)
+                    for trg_id in range(self.num_targets):
+                        if self.enable_metric_losses:
+                            ml_loss_module = self.ml_losses[trg_id][model_name]
+                            ml_loss_module.end_iteration(do_backward=False)
+                    if not self.enable_sam and step == 1:
+                        self.scaler.step(self.optims[model_name])
+                        self.scaler.update()
+                    elif step == 1:
+                        assert self.enable_sam
+                        if self.clip_grad == 0:
+                            # if self.clip_grad == 0  this means that unscale_ wasn't applied,
+                            # so we manually unscale the parameters to perform SAM manipulations
+                            self.scaler.unscale_(self.optims[model_name]) 
+                        overflow = self.optims[model_name].first_step()
+                        self.scaler.update() # update scaler after first step
+                        if overflow:
+                            print("Overflow occurred. Skipping step ...")
+                            loss_summary['loss'] = total_loss.item()
+                            # skip second step  if overflow occurred 
+                            return loss_summary, avg_acc
+                    else:
+                        assert self.enable_sam and step==2
+                        # unscale the parameters to perform SAM manipulations
+                        self.scaler.unscale_(self.optims[model_name])
+                        self.optims[model_name].second_step()
+                        self.scaler.update()
 
-                    with torch.no_grad():
-                        trg_probs = torch.softmax(torch.stack(out_logits[trg_id]), dim=2).mean(dim=0)
+                loss_summary['loss'] = total_loss.item()
 
-                    for model_id, logits in enumerate(out_logits[trg_id]):
-                        log_probs = torch.log_softmax(logits, dim=1)
-                        m_loss = (trg_probs * log_probs).sum(dim=1).mean().neg()
-
-                        mutual_loss += m_loss
-                        loss_summary['mutual_{}/{}'.format(trg_id, model_names[model_id])] = m_loss.item()
-                        num_mutual_losses += 1
-
-                should_turn_off_mutual_learning = self._should_turn_off_mutual_learning(self.epoch)
-                coeff_mutual_learning = int(not should_turn_off_mutual_learning)
-
-                total_loss += coeff_mutual_learning * mutual_loss / float(num_mutual_losses)
-                if self.compression_ctrl:
-                    compression_loss = self.compression_ctrl.loss()
-                    loss_summary['compression_loss'] = compression_loss
-                    total_loss += compression_loss
-
-            # backward pass
-            self.scaler.scale(total_loss).backward(retain_graph=self.enable_metric_losses)
-
-            for model_name in model_names:
-                if not self.models[model_name].training:
-                    continue
-                if self.clip_grad != 0 and step == 1:
-                    self.scaler.unscale_(self.optims[model_name])
-                    torch.nn.utils.clip_grad_norm_(self.models[model_name].parameters(), self.clip_grad)
-                for trg_id in range(self.num_targets):
-                    if self.enable_metric_losses:
-                        ml_loss_module = self.ml_losses[trg_id][model_name]
-                        ml_loss_module.end_iteration(do_backward=False)
-                if not self.enable_sam and step == 1:
-                    self.scaler.step(self.optims[model_name])
-                    self.scaler.update()
-                elif step == 1:
-                    assert self.enable_sam
-                    if self.clip_grad == 0:
-                        # if self.clip_grad == 0  this means that unscale_ wasn't applied,
-                        # so we manually unscale the parameters to perform SAM manipulations
-                        self.scaler.unscale_(self.optims[model_name]) 
-                    overflow = self.optims[model_name].first_step()
-                    self.scaler.update() # update scaler after first step
-                    if overflow:
-                        print("Overflow occurred. Skipping step ...")
-                        loss_summary['loss'] = total_loss.item()
-                        # skip second step  if overflow occurred 
-                        return loss_summary, avg_acc
-                else:
-                    assert self.enable_sam and step==2
-                    # unscale the parameters to perform SAM manipulations
-                    self.scaler.unscale_(self.optims[model_name])
-                    self.optims[model_name].second_step()
-                    self.scaler.update()
-
-            loss_summary['loss'] = total_loss.item()
-
-        return loss_summary, avg_acc
+            return loss_summary, avg_acc
 
     def _single_model_losses(self, model, train_records, imgs, obj_ids, n_iter, model_name, num_packages):
         run_kwargs = self._prepare_run_kwargs(obj_ids)
@@ -403,8 +404,8 @@ class ImageAMSoftmaxEngine(Engine):
                         neg_mask = ~pos_mask
 
                         trg_mask_values = torch.where(pos_mask,
-                                                      torch.ones_like(pos_float_mask),
-                                                      torch.zeros_like(pos_float_mask))
+                                                    torch.ones_like(pos_float_mask),
+                                                    torch.zeros_like(pos_float_mask))
                         num_positives = trg_mask_values.sum(dim=(1, 2, 3), keepdim=True)
                         num_negatives = float(att_map_size[0] * att_map_size[1]) - num_positives
 

torchreid/models/timm_wrapper.py
@@ -38,7 +38,6 @@ class TimmModelsWrapper(ModelInterface):
                              else self.model.num_features)
         self.dropout = Dropout(**dropout_cls)
         self.pooling_type = pooling_type
-        self.forward = autocast(self.mix_precision)(self.forward)
         if self.loss in ["am_softmax", "am_binary"]:
             self.model.act2 = nn.PReLU()
             self.classifier = AngleSimpleLinear(self.num_features, num_classes)
@@ -47,14 +46,15 @@ class TimmModelsWrapper(ModelInterface):
             self.classifier = self.model.get_classifier()
 
     def forward(self, x, return_featuremaps=False, **kwargs):
-        y = self.extract_features(x)
-        if return_featuremaps:
-            return y
-        glob_features = self._glob_feature_vector(y, self.pooling_type, reduce_dims=False)
-        logits = self.infer_head(glob_features)
-        if not self.training:
-            return [logits]
-        return tuple([logits])
+        with autocast(enabled=self.mix_precision):
+            y = self.extract_features(x)
+            if return_featuremaps:
+                return y
+            glob_features = self._glob_feature_vector(y, self.pooling_type, reduce_dims=False)
+            logits = self.infer_head(glob_features)
+            if not self.training:
+                return [logits]
+            return tuple([logits])
 
     def extract_features(self, x):
         if self.is_mobilenet: