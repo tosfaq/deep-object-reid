2492b0489ba2a974c8f0e2458c8b231f31cd578f
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-10-14 20:20:15 +0300

    remove unused attrs for pot model

--- tools/sc_sample.py
+++ tools/sc_sample.py
@@ -154,13 +154,6 @@ def main(args):
         optimized_model = ModelEntity(
             dataset,
             environment.get_model_configuration(),
-            optimization_type=ModelOptimizationType.POT,
-            optimization_methods=OptimizationMethod.QUANTIZATION,
-            optimization_objectives={},
-            precision=[ModelPrecision.INT8],
-            target_device=TargetDevice.CPU,
-            performance_improvement={},
-            model_size_reduction=1.,
             model_status=ModelStatus.NOT_READY)
         openvino_task.optimize(
             OptimizationType.POT,
