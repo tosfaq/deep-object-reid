64ce80d3aa1a3852d65613b961d05000bab439bb
aiarkinx <antonx.iarkin@intel.com>
2021-12-03 12:59:49 +0300

    template path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 import e2e.fixtures
 
 from e2e.conftest_utils import * # noqa
-from e2e.conftest_utils import pytest_addoption as _e2e_pytest_addoption # noqa
-from e2e import config # noqa
 from e2e.utils import get_plugins_from_packages
 pytest_plugins = get_plugins_from_packages([e2e])
 
@@ -14,3 +12,5 @@ def pytest_addoption(parser):
                      help='Optional. If the parameter is set, it filters test_ote_training tests by usecase field.')
     parser.addoption('--expected-metrics-file', action='store', default=None,
                      help='Optional. If the parameter is set, it points the YAML file with expected test metrics.')
+    parser.addoption('--template-paths', action='store', default=None,
+                     help='Path to the template_paths.yml file for tests that require templates.')
