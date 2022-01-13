295ccd42b1ae5630b527ec4e5e8a5650b7705fe8
kprokofi <kirill.prokofiev@intel.com>
2022-01-13 23:44:06 +0300

    remove print

--- torchreid/integration/sc/inference_task.py
+++ torchreid/integration/sc/inference_task.py
@@ -154,8 +154,6 @@ class OTEClassificationInferenceTask(IInferenceTask, IEvaluationTask, IExportTas
             config_file_path = os.path.join(base_dir, 'main_model_multilabel.yaml')
         else:
             config_file_path = os.path.join(base_dir, 'main_model.yaml')
-        print(self._cfg.train.keys())
-
         merge_from_files_with_base(self._cfg, config_file_path)
         self._cfg.use_gpu = torch.cuda.device_count() > 0
         self.num_devices = 1 if self._cfg.use_gpu else 0
