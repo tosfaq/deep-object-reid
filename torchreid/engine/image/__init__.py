ec2af1445c291b6c41a13b221c8f0dd535074b1a
kprokofi <kirill.prokofiev@intel.com>
2022-01-17 16:44:42 +0300

    start to change engines. Remove triplet and contrastive

--- torchreid/engine/image/__init__.py
+++ torchreid/engine/image/__init__.py
@@ -8,6 +8,4 @@
 from __future__ import absolute_import
 
 from .am_softmax import ImageAMSoftmaxEngine
-from .contrastive import ImageContrastiveEngine
 from .multilabel import MultilabelEngine
-from .triplet import ImageTripletEngine
