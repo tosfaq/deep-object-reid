2f23e5ef27489d7fd56ecced8f25a14730cc4195
kprokofi <kirill.prokofiev@intel.com>
2022-02-08 13:11:09 +0300

    revert loss update

--- torchreid/losses/asl.py
+++ torchreid/losses/asl.py
@@ -76,7 +76,7 @@ class AMBinaryLoss(nn.Module):
         # prevent memory allocation and gpu uploading every iteration, and encourages inplace operations
         self.anti_targets = self.xs_pos = self.xs_neg = self.asymmetric_w = self.loss = None
 
-    def forward(self, cos_theta, targets, scale=None, aug_index=None, lam=None):
+    def forward(self, cos_theta, targets, aug_index=None, lam=None, scale=None):
         """"
         Parameters
         ----------
@@ -84,36 +84,32 @@ class AMBinaryLoss(nn.Module):
         targets: targets (multi-label binarized vector)
         """
         self.s = scale if scale else self.s
+        print(self.s)
         if self.label_smooth > 0:
             targets = targets * (1 - self.label_smooth)
             targets[targets == 0] = self.label_smooth
+
         self.anti_targets = 1 - targets
 
         if self.asymmetric_focus:
             # Calculating Probabilities
-            xs_pos = torch.sigmoid(self.s * (cos_theta - self.m))
-            xs_neg = torch.sigmoid(- self.s * (cos_theta + self.m))
-            # Asymmetric Probability Shifting
-            if self.clip is not None and self.clip > 0:
-                xs_neg = (xs_neg + self.clip).clamp(max=1)
-            pt0 = xs_neg * targets
-            pt1 = xs_pos * (1 - targets)
-            pt = pt0 + pt1
-            one_sided_gamma = self.gamma_pos * targets + self.gamma_neg * (1 - targets)
-            # P_pos ** gamm_neg * (...) + P_neg ** gamma_pos * (...)
-            one_sided_w = torch.pow(pt, one_sided_gamma)
-            balance_koeff_pos = self.k / self.s
-            balance_koeff_neg = (1 - self.k) / self.s
+            self.xs_pos = torch.sigmoid(self.s * (cos_theta - self.m))
+            self.xs_neg = torch.sigmoid(self.s * (-cos_theta - self.m))
+            balance_koeff_pos = 1
+            balance_koeff_neg = 1
         else:
             assert not self.asymmetric_focus
-            # SphereFace2 balancing coefficients
             balance_koeff_pos = self.k / self.s
             balance_koeff_neg = (1 - self.k) / self.s
+        self.loss = balance_koeff_pos * targets * torch.log(1 + torch.exp(-self.s * (cos_theta - self.m)))
+        self.loss.add_(balance_koeff_neg * self.anti_targets * torch.log(1 + torch.exp(self.s * (cos_theta + self.m))))
 
-        self.loss = balance_koeff_pos * targets * F.logsigmoid(self.s * (cos_theta - self.m))
-        self.loss.add_(balance_koeff_neg * self.anti_targets * F.logsigmoid(- self.s * (cos_theta + self.m)))
-
+        # Asymmetric Focusing
         if self.asymmetric_focus:
-            self.loss *= one_sided_w
+            self.xs_pos = self.xs_pos * targets
+            self.xs_neg = self.xs_neg * self.anti_targets
+            self.asymmetric_w = torch.pow(1 - self.xs_pos - self.xs_neg,
+                                          self.gamma_pos * targets + self.gamma_neg * self.anti_targets)
+            self.loss *= self.asymmetric_w
 
-        return - self.loss.sum() / cos_theta.size(0)
+        return self.loss.sum()
