cb260c35525785cae24e54862f2b94dfb3a1a18d
Sungchul Kim <sungchul.kim@intel.com>
2022-05-17 14:20:28 +0900

    fix if order which defines output fc layer (#220)
    * fix if order which defines output fc layer
    
    * add valueerror
    
    * fix

--- torchreid/models/efficient_net_pytcv.py
+++ torchreid/models/efficient_net_pytcv.py
@@ -351,15 +351,16 @@ class EfficientNet(ModelInterface):
         self.output = nn.Sequential()
         if dropout_cls:
             self.output.add_module("dropout", Dropout(**dropout_cls))
-        if 'softmax' in self.loss or 'asl' in self.loss:
+        if 'am_softmax' == self.loss or 'am_binary' == self.loss:
+            self.output.add_module("asl", AngleSimpleLinear(
+                in_features=final_block_channels,
+                out_features=self.num_classes))
+        elif 'softmax' == self.loss or 'asl' == self.loss:
             self.output.add_module("fc", nn.Linear(
                 in_features=final_block_channels,
                 out_features=self.num_classes))
         else:
-            assert 'am_softmax' in self.loss or 'am_binary' in self.loss
-            self.output.add_module("asl", AngleSimpleLinear(
-                in_features=final_block_channels,
-                out_features=self.num_classes))
+            raise ValueError(f"self.loss={self.loss} is unknown loss type.")
 
         self._init_params()
 
